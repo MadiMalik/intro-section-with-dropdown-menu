398e76ec5622a7a20803098577bf38ce81b96e1a
MadiMalik <119412836+MadiMalik@users.noreply.github.com>
2024-01-15 16:25:16 +0530

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 import './App.css';
-import logo from './images/logo.svg';
+import Menu from './components/Menu';
 import openMenu from './images/icon-menu.svg';
 import closeMenu from './images/icon-close-menu.svg';
 import clientImg1 from './images/client-databiz.svg';
@@ -9,98 +8,34 @@ import clientImg3 from './images/client-meet.svg';
 import clientImg4 from './images/client-maker.svg';
 import mobile from './images/image-hero-mobile.png';
 import desktop from './images/image-hero-desktop.png';
-import arrowDown from './images/icon-arrow-down.svg';
-import arrowUp from './images/icon-arrow-up.svg';
-
-import todoList from './images/icon-todo.svg'
-import calender from './images/icon-calendar.svg'
-import reminder from './images/icon-reminders.svg'
-import planning from './images/icon-planning.svg'
 
 
 
 function App() {
   const handleOpenMenu = () => {
-    const navbar = document.querySelector(".navbar")
-    const menuButton = document.querySelector('.menu-button');
-    navbar.classList.toggle('open')
-
-    if(navbar.classList.contains("open")){
-      menuButton.src = closeMenu
-    }else{
-      menuButton.src = openMenu
-    }
+    const overlay = document.querySelector(".overlay")
+    overlay.classList.toggle("show")
   }
 
   return (
     <>
-      <div className="wrapper w-full bg-white ">
-        <header className='w-full flex justify-between items-center pt-6 pb-6 px-8  lg:justify-start'>
-          <div>
-            <img src={logo} alt="Logo " />
-
-            <nav className='navbar '>
-              <div>
-                <button className='flex items-center justify-start'>
-                  Features <img src={arrowDown} alt="Arrow down" className='ml-2'/>
-                </button>
-
-                <ul className='mt-2'>
-                  <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={todoList} alt="" />Todo List</li>
-                  <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={calender} alt="" />Calender</li>
-                  <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={reminder} alt="" />Reminders</li>
-                  <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={planning} alt="" />Planning</li>
-                </ul>
-              </div>
-
-              <div>
-                <button className='flex items-center justify-start'>
-                  Company <img src={arrowDown} alt="Arrow down" className='ml-2' />
-                </button>
-
-                <ul className='mt-2 ml-3'>
-                  <li className='flex items-center justify-start text-sm mb-2'>History</li>
-                  <li className='flex items-center justify-start text-sm mb-2'>Our Team</li>
-                  <li className='flex items-center justify-start text-sm mb-2'>Blog</li>
-                </ul>
-              </div>
-
-              <div className='mb-2 lg:mb-0'>
-                <button>Careers</button>
-              </div>
-
-              <div>
-                <button>About</button>
-              </div>
-            </nav>
-          </div>
-
-          <div>
-            <button>
-              <img className='menu-button' onClick={handleOpenMenu} src={openMenu} alt="menu icon" />
-            </button>
-          </div>
-
-          <div className='hidden lg:block'>
-            <button>Login</button>
-            <button>Register</button>
-          </div>
-        </header>
-
-        <main className='flex flex-col-reverse py-20 md:grid md:gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto'>
+      <div className="wrapper">
+        <Menu/>
+        
+        <main className='flex flex-col-reverse py-10 lg:py-8 md:grid md:gap-[10rem] md:grid-cols-2 md:place-items-center mx-4xl md:px-[8rem] lg:px-[10rem] mx-auto'>
         
           <section className="text-center md:text-left px-5 xl:px-0 mt-10 md:mt-0">
             <h1 className='font-bold text-5xl lg:text-8xl mb-5'>
-              Make remote work
+              Make <br className='hidden lg:block'/> remote work
             </h1>
-            <p className="mb-7">
+            <p className="mb-7 lg:mt-12">
               Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.
             </p>
 
-            <a className='bg-black-500 rounded-lg shadow text-gray-200 font-bold hover:opacity-75 transition-all duration-150 pt-3 pb-3 px-8 cursor-pointer'
+            <a className='bg-black-500 rounded-lg shadow text-gray-200 font-bold hover:opacity-75 transition-all duration-150 pt-3 pb-3 px-8 mt-4 cursor-pointer lg:mt-12'
              href="#">Learn more</a>
 
-            <ul className='grid grid-cols-4 gap-1 place-items-center mt-10 md:place-items-start'>
+            <ul className='grid grid-cols-4 gap-1 place-items-center mt-12 lg:mt-10 md:place-items-start lg:pt-12'>
               <li><img src={clientImg1} className='w-16 md:w-24' alt="" /></li>
               <li><img src={clientImg2} className='w-16 md:w-24' alt="" /></li>
               <li><img src={clientImg3} className='w-16 md:w-24' alt="" /></li>
@@ -111,11 +46,13 @@ function App() {
           <section>
             <picture>
               <source media="(min-width: 768px)" srcSet={desktop} />
-              <img src={mobile} alt="" className="w-full" />
+              <img src={mobile} alt="" className="w-full lg:h-min" />
             </picture>
           </section>
         </main>
       </div>
+
+      <div className="overlay" onClick={handleOpenMenu}></div>
     </>
   )
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,100 @@
+// import React from 'react'
+import { useState } from 'react';
+import openMenu from '../images/icon-menu.svg';
+import closeMenu from '../images/icon-close-menu.svg';
+
+import logo from '../images/logo.svg';
+import arrowDown from '../images/icon-arrow-down.svg';
+import todoList from '../images/icon-todo.svg'
+import calender from '../images/icon-calendar.svg'
+import reminder from '../images/icon-reminders.svg'
+import planning from '../images/icon-planning.svg'
+
+import arrowUp from '../images/icon-arrow-up.svg';
+
+// rfce
+function Menu() {
+    const [openFeatures, setOpenFeatures] = useState(false)
+    const [openCompany, setOpenCompany] = useState(false)
+
+    const handleOpenMenu = () => {
+      const navbar = document.querySelector(".navbar")
+      const overlay = document.querySelector(".overlay")
+      const menuButton = document.querySelector('.menu-button');
+      navbar.classList.toggle('open')
+      overlay.classList.toggle("show")
+  
+      if(navbar.classList.contains("open")){
+        menuButton.src = closeMenu
+      }else{
+        menuButton.src = openMenu
+      }
+    }
+  
+    return (
+      <>
+          <header className='w-full flex justify-between items-center pt-10 lg:pt-8 pb-6 px-8'>
+            <div className='lg:flex lg:items-start lg:justify-start lg:gap-8'>
+              <img className='lg:mr-12' src={logo} alt="Logo " />
+  
+              <nav className='navbar lg:gap-8'>
+                <div >
+                  <button onClick={ () => setOpenFeatures(!openFeatures)} className='flex items-center justify-start hover:text-gray-300'>
+
+                    Features {openFeatures ? <img src={arrowUp} alt="Arrow down" className='ml-4'/> : <img src={arrowDown} alt="Arrow down" className='ml-4 hover:text-gray-300'/>}
+                  </button>
+                  {openFeatures && 
+                    <ul className='mt-2 ml-3 lg:ml-0 lg:p-3 lg:shadow'>
+                    <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={todoList} alt="" />Todo List</li>
+                    <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={calender} alt="" />Calender</li>
+                    <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={reminder} alt="" />Reminders</li>
+                    <li className='flex items-center justify-start text-sm mb-2'><img className='mr-2' src={planning} alt="" />Planning</li>
+                  </ul>
+                  }
+                </div>
+  
+                <div className='mt-4 lg:mt-0'>
+                  <a onClick={() =>setOpenCompany(!openCompany)} className='flex items-center justify-start hover:text-gray-300'>
+                    Company {openCompany ?  <img src={arrowUp} alt="Arrow Up" className='ml-4' /> :  <img src={arrowDown} alt="Arrow down" className='ml-4' />}
+                  </a>
+  
+                  {openCompany && <ul className='mt-2 ml-3 lg:ml-0 lg:p-3 lg:shadow'>
+                    <li className='flex items-center justify-start text-sm mb-2'>History</li>
+                    <li className='flex items-center justify-start text-sm mb-2'>Our Team</li>
+                    <li className='flex items-center justify-start text-sm mb-2'>Blog</li>
+                  </ul>}
+                </div>
+  
+                <div className='mb-2 lg:mb-0 mt-6 lg:mt-0 hover:text-gray-300'>
+                  <a>Careers</a>
+                </div>
+  
+                <div className='mt-4 lg:mt-0 hover:text-gray-300'>
+                  <a>About</a>
+                </div>
+
+                <div className='grid mt-16 lg:hidden'>
+                  <a className='opacity-75 hover:text-black-500'>Login</a>
+                  <a className='border-2 border-gray-300 opacity-75 py-2 rounded-3xl mt-6 hover:bg-black-500 transition-all'>Register</a>
+                </div>
+              </nav>
+            </div>
+  
+            <div className='lg:hidden'>
+              <a>
+                <img className='menu-button' onClick={handleOpenMenu} src={openMenu} alt="menu icon" />
+              </a>
+            </div>
+  
+            <div className='hidden lg:block'>
+              <a className='mr-10 opacity-75 hover:text-gray-300'>Login</a>
+              <a className='border-2 border-black-500 px-6 py-2 rounded-3xl hover:bg-black-500 hover:text-gray-200'>Register</a>
+            </div>
+          </header>
+
+          <div className="overlay" onClick={handleOpenMenu}></div>
+    </>
+  )
+}
+
+export default Menu
